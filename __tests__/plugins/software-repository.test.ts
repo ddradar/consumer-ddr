851efe0819683fdfad95e7171163cf6b68f79ba6
nogic <24802730+nogic1008@users.noreply.github.com>
2020-02-24 23:41:40 +0900

    feat: add getSoftwareInfo & test

--- __tests__/plugins/software-repository.test.ts
+++ __tests__/plugins/software-repository.test.ts
@@ -47,4 +47,53 @@ describe('plugins/software-repository.ts', () => {
       })
     })
   })
+
+  describe('getSoftwareInfo', () => {
+    test('returns undefined if not found', async () => {
+      // Arrange
+      jest.mock('~/series')
+
+      // Act
+      const info = (
+        await import('~/plugins/software-repository')
+      ).getSoftwareInfo('hoge')
+
+      // Assert
+      expect(info).toBe(undefined)
+    })
+    test('returns SoftwareInfo if found', async () => {
+      // Arrange
+      jest.mock('~/series', () => [mockSoftware])
+
+      // Act
+      const info = (
+        await import('~/plugins/software-repository')
+      ).getSoftwareInfo(mockSoftware.id)
+
+      // Assert
+      expect(info).toStrictEqual({
+        id: 'supernova2-jp',
+        name: 'Dance Dance Revolution SuperNOVA2',
+        region: 'JP',
+        launched: new Date('2017-02-01'),
+        platform: 'Play Station 2',
+        songs: [
+          {
+            id: 'paranoia-180',
+            name: 'PARANOiA',
+            artist: '180',
+            bpm: 180,
+            charts: [
+              {
+                songId: 'paranoia-180',
+                playStyle: 'SINGLE',
+                difficulty: 1,
+                level: 6
+              }
+            ]
+          }
+        ]
+      })
+    })
+  })
 })
