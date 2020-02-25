df23188e89cc2a5a69b84f4d8d7c408aff11f4a3
Nogic <24802730+nogic1008@users.noreply.github.com>
2020-02-25 10:16:57 +0900

    test: add series/index.ts test (#25)
    * test: add /series/index.ts test
    * test: widen collect coverage
    * chore(deps-dev): bump jest from 24.9.0 to 25.1.0

--- __tests__/series/json.test.ts
+++ __tests__/series/json.test.ts
@@ -1,6 +1,7 @@
-import * as fs from 'fs'
+import { readdirSync, readFile } from 'fs'
 import { matchersWithOptions } from 'jest-json-schema'
 import * as path from 'path'
+import { promisify } from 'util'
 
 import softwareSchema from '~/static/software-schema.json'
 
@@ -9,17 +10,25 @@ expect.extend(
     schemas: [softwareSchema]
   })
 )
+const rootPath = path.join(__dirname, '..', '..')
+const readFileAsync = promisify(readFile)
 
-describe('/series/*.json', () => {
-  const songsDirPath = path.join(__dirname, '..', '..', 'series')
-  const jsonFilesPath = fs
-    .readdirSync(songsDirPath)
-    .filter((p) => path.extname(p) === '.json')
-  const jsonStrings = jsonFilesPath.map((filePath) => {
-    return fs.readFileSync(path.join(songsDirPath, filePath), 'utf8')
-  })
-  test.each(jsonStrings)('should be valid json', (s) => {
-    const jsonObject = JSON.parse(s)
+describe('/series/', () => {
+  const dirPath = path.join(rootPath, 'series')
+  const jsonFiles: string[] = readdirSync(dirPath).filter(
+    (p) => path.extname(p) === '.json'
+  )
+
+  test.each(jsonFiles)('%s should be valid json', async (s) => {
+    const jsonString = await readFileAsync(path.join(dirPath, s), 'utf8')
+    const jsonObject = JSON.parse(jsonString)
     expect(jsonObject).toMatchSchema(softwareSchema)
+  })
+
+  describe('index.ts', () => {
+    test('export all JSONs located /series/', async () => {
+      const list = (await import('~/series/')).default
+      expect(list).toHaveLength(jsonFiles.length)
+    })
   })
 })
