2de62fc612b22c34bb7074316309d6740741e533
nogic <24802730+nogic1008@users.noreply.github.com>
2020-03-02 19:16:45 +0900

    feat: simplify default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,20 +19,7 @@
     </nav>
 
     <section class="main-content columns">
-      <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">
-          General
-        </p>
-        <ul class="menu-list">
-          <li v-for="(item, key) of items" :key="key">
-            <nuxt-link :to="item.to" exact-active-class="is-active">
-              <b-icon :icon="item.icon" /> {{ item.title }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </aside>
-
-      <div class="container column is-10">
+      <div class="container column is-12">
         <nuxt />
       </div>
     </section>
@@ -43,13 +30,5 @@
 import { Component, Vue } from 'vue-property-decorator'
 
 @Component
-export default class DefaultLayout extends Vue {
-  items = [
-    {
-      title: 'Home',
-      icon: 'home',
-      to: { name: 'index' }
-    }
-  ]
-}
+export default class DefaultLayout extends Vue {}
 </script>
