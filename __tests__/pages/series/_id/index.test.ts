dfc6e10262b216924b3c526df2e94fca0964f1e1
Nogic <24802730+nogic1008@users.noreply.github.com>
2020-03-08 13:32:32 +0900

    fix(test): collect coverage from *.vue files (#37)
    * chore(deps-dev): rollback jest version to 24
    * test: except *.d.ts from coverage
    * test: add page test case

--- __tests__/pages/series/_id/index.test.ts
+++ __tests__/pages/series/_id/index.test.ts
@@ -1,14 +1,18 @@
+import { Context } from '@nuxt/types'
 import { createLocalVue, mount, shallowMount, Wrapper } from '@vue/test-utils'
 import Buefy from 'buefy'
+import { mocked } from 'ts-jest/utils'
 
 import SeriesDetail from '~/pages/series/_id/index.vue'
-import { SoftwareInfo } from '~/plugins/software-repository'
+import { getSoftwareInfo, SoftwareInfo } from '~/plugins/software-repository'
 
 const localVue = createLocalVue()
 localVue.use(Buefy)
+jest.mock('~/plugins/software-repository')
 
 describe('pages/series/_id/index.vue', () => {
   let wrapper: Wrapper<Vue>
+  let vm: any
   const info: SoftwareInfo = {
     id: '1st-jp',
     name: 'Dance Dance Revolution',
@@ -145,17 +149,104 @@ describe('pages/series/_id/index.vue', () => {
       localVue,
       data: () => ({ info, chartRows })
     })
+    vm = wrapper.vm
   })
 
   test('is a Vue instance', () => {
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
-
   test('renders correctly', () => {
     const wrapper = mount(SeriesDetail, {
       localVue,
       data: () => ({ info, chartRows })
     })
     expect(wrapper.element).toMatchSnapshot()
+  })
+  describe('asyncData()', () => {
+    let asyncData: ({
+      params,
+      payload
+    }: Pick<Context, 'params' | 'payload'>) => any
+    beforeEach(() => (asyncData = vm.$options.asyncData))
+    afterEach(() => mocked(getSoftwareInfo).mockReset())
+
+    test('returns default value', async () => {
+      // Arrange
+      mocked(getSoftwareInfo).mockReturnValue(undefined)
+
+      // Act
+      const data = await asyncData({
+        params: { id: 'foo' },
+        payload: undefined
+      })
+
+      expect(data).toStrictEqual({ info: undefined, chartRows })
+      expect(mocked(getSoftwareInfo).mock.calls).toHaveLength(1)
+      expect(mocked(getSoftwareInfo).mock.calls[0][0]).toBe('foo')
+    })
+    test('returns payload value', async () => {
+      // Arrange
+      mocked(getSoftwareInfo).mockReturnValue(undefined)
+
+      // Act
+      const data = await asyncData({ params: {}, payload: info })
+
+      // Assert
+      expect(mocked(getSoftwareInfo).mock.calls).toHaveLength(0)
+      expect(data).toStrictEqual({ info, chartRows })
+    })
+    test('returns getSoftwareInfo(params.id) value', async () => {
+      // Arrange
+      mocked(getSoftwareInfo).mockReturnValue(info)
+
+      // Act
+      const data = await asyncData({
+        params: { id: '1st-jp' },
+        payload: undefined
+      })
+
+      // Assert
+      expect(mocked(getSoftwareInfo).mock.calls).toHaveLength(1)
+      expect(mocked(getSoftwareInfo).mock.calls[0][0]).toBe('1st-jp')
+      expect(data).toStrictEqual({ info, chartRows })
+    })
+  })
+  describe('tooltip()', () => {
+    test.each([0, 1, -1, 0.1, -0.1, NaN, Infinity, -Infinity])(
+      'returns "Unknown" if info is undefined',
+      (i) => {
+        // Arrange
+        wrapper = shallowMount(SeriesDetail, {
+          localVue,
+          data: () => ({ info: undefined, chartRows })
+        })
+        const tooltip: (i: number) => string = (wrapper.vm as any).tooltip
+
+        // Act & Assert
+        expect(tooltip(i)).toBe('Unknown')
+      }
+    )
+    test.each([
+      ['BASIC', 1],
+      ['ANOTHER', 2],
+      ['MANIAC', 3],
+      ['NORMAL', 5]
+    ])('returns "%s" if difficulty is %i', (expected, i) => {
+      // Arrange
+      const tooltip: (i: number) => string = (wrapper.vm as any).tooltip
+
+      // Act & Assert
+      expect(tooltip(i)).toBe(expected)
+    })
+    test.each([4, -1, 0.1, 0.5, NaN, Infinity, -Infinity])(
+      'returns "Unknown" if difficulty is %i',
+      (i) => {
+        // Arrange
+        const tooltip: (i: number) => string = (wrapper.vm as any).tooltip
+
+        // Act & Assert
+        expect(tooltip(i)).toBe('Unknown')
+      }
+    )
   })
 })
