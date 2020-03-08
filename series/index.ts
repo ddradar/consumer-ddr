ddc111e564326ed4c904effdf800a91b0797a821
nogic <24802730+nogic1008@users.noreply.github.com>
2020-03-08 10:59:10 +0900

    fix: add software json type def

--- series/index.ts
+++ series/index.ts
@@ -1,5 +1,18 @@
+import { Chart } from '~/types/chart'
+import { Song } from '~/types/song'
+
 import firstJp from './1st-jp.json'
 import secondRemixJp from './2nd-remix-jp.json'
 
-const softwareList = [firstJp, secondRemixJp]
+type SoftWareJson = {
+  id: string
+  name: string
+  platform: string
+  region: string
+  launched: string
+  songs: (Song & { charts: Omit<Chart, 'songId'>[] })[]
+  difficultyNames: { [key: number]: string }
+}
+
+const softwareList = [firstJp, secondRemixJp] as SoftWareJson[]
 export default softwareList
