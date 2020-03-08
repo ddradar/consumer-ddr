1ed28d6af8091f13a6ecd16d6d18baea02ca58d4
nogic <24802730+nogic1008@users.noreply.github.com>
2020-03-08 21:42:51 +0900

    feat: add software list in index

--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,31 @@
 <template>
   <section class="section">
-    <div class="columns is-mobile">
-      <card title="Free" icon="github-circle">
-        Open source on
-        <a href="https://github.com/buefy/buefy">
-          GitHub
-        </a>
-      </card>
+    <b-table :data="softwareList" striped narrowed mobile-cards>
+      <template slot-scope="props">
+        <b-table-column field="name" label="Name" searchable>
+          <nuxt-link class="is-size-6-mobile" :to="`/series/${props.row.id}/`">
+            {{ props.row.name }}
+          </nuxt-link>
+        </b-table-column>
+        <b-table-column field="platform" label="Platform" searchable>
+          <span>
+            {{ props.row.platform }}
+            {{ getRegionFlag(props.row.region) }}
+          </span>
+        </b-table-column>
+        <b-table-column field="launched" label="Launched">
+          {{ props.row.launched.toLocaleDateString() }}
+        </b-table-column>
+      </template>
 
-      <card title="Responsive" icon="cellphone-link">
-        <b class="has-text-grey">
-          Every
-        </b>
-        component is responsive
-      </card>
-
-      <card title="Modern" icon="alert-decagram">
-        Built with
-        <a href="https://vuejs.org/">
-          Vue.js
-        </a>
-        and
-        <a href="http://bulma.io/">
-          Bulma
-        </a>
-      </card>
-
-      <card title="Lightweight" icon="arrange-bring-to-front">
-        No other internal dependency
-      </card>
-    </div>
+      <template slot="empty">
+        <section class="section">
+          <div class="content has-text-grey has-text-centered">
+            <p>Nothing here.</p>
+          </div>
+        </section>
+      </template>
+    </b-table>
   </section>
 </template>
 
@@ -37,10 +33,26 @@
 import { Component, Vue } from 'vue-property-decorator'
 
 import Card from '~/components/Card.vue'
+import { getSoftwareList } from '~/plugins/software-repository'
+import { Region } from '~/types/software'
 
 @Component({
   name: 'HomePage',
   components: { Card }
 })
-export default class IndexPage extends Vue {}
+export default class IndexPage extends Vue {
+  get softwareList() {
+    return getSoftwareList()
+  }
+
+  getRegionFlag(region: Region) {
+    return region === 'JP'
+      ? '\u{1F1EF}\u{1F1F5}'
+      : region === 'US'
+      ? '\u{1F1FA}\u{1F1F8}'
+      : region === 'EU'
+      ? '\u{1F1EA}\u{1F1FA}'
+      : '?'
+  }
+}
 </script>
