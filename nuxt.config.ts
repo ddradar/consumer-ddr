4ededb08123e9bdf7d794294683b0602a411bbea
Nogic <24802730+nogic1008@users.noreply.github.com>
2020-03-08 17:16:22 +0900

    build: generate dynamic route (#39)
    * refactor: avoid use short path
    * build: add generate option
    * refactor: remove comment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,10 @@
 import { Configuration } from '@nuxt/types'
 
 import pkg from './package.json'
+import { getSoftwareInfo, getSoftwareList } from './plugins/software-repository'
 
 const config: Configuration = {
   mode: 'universal',
-  /*
-   ** Headers of the page
-   */
   head: {
     title: 'Consumer DDR',
     meta: [
@@ -16,31 +14,28 @@ const config: Configuration = {
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
-  /*
-   ** Customize the progress-bar color
-   */
   loading: { color: '#fff' },
-  /*
-   ** Global CSS
-   */
   css: [],
-  /*
-   ** Nuxt.js dev-modules
-   */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
     // Doc: https://typescript.nuxtjs.org/
     '@nuxt/typescript-build'
   ],
-  /*
-   ** Nuxt.js modules
-   */
   modules: [
     // Doc: https://buefy.org/documentation/
     'nuxt-buefy',
     '@nuxtjs/pwa'
-  ]
+  ],
+  generate: {
+    routes() {
+      const softwareList = getSoftwareList().map((s) => s.id)
+      return softwareList.map((id) => ({
+        route: `/series/${id}`,
+        payload: getSoftwareInfo(id)
+      }))
+    }
+  }
 }
 
 export default config
