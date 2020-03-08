da3e0b6e23c1e2c8672cb8a2b261cb5fbf4de1ac
nogic <24802730+nogic1008@users.noreply.github.com>
2020-03-08 18:18:00 +0900

    feat: add icon & pwa settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,19 +3,44 @@ import { Configuration } from '@nuxt/types'
 import pkg from './package.json'
 import { getSoftwareInfo, getSoftwareList } from './plugins/software-repository'
 
+const title = 'Consumer DDR'
+
 const config: Configuration = {
   mode: 'universal',
   head: {
-    title: 'Consumer DDR',
+    titleTemplate: `%s - ${title}`,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', sizes: '16x16', type: 'image/png', href: '/icon-16.png' },
+      { rel: 'icon', sizes: '32x32', type: 'image/png', href: '/icon-32.png' },
+      { rel: 'icon', sizes: '48x48', type: 'image/png', href: '/icon-48.png' },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png'
+      },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
   },
   loading: { color: '#fff' },
   css: [],
+  pwa: {
+    icon: {
+      iconFileName: 'icon-1024.png'
+    },
+    manifest: {
+      name: title,
+      short_name: 'CS DDR',
+      description: pkg.description,
+      background_color: '#fff',
+      theme_color: '#7957d5',
+      orientation: 'any'
+    }
+  },
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
@@ -25,6 +50,7 @@ const config: Configuration = {
   modules: [
     // Doc: https://buefy.org/documentation/
     'nuxt-buefy',
+    // Doc: https://pwa.nuxtjs.org/
     '@nuxtjs/pwa'
   ],
   generate: {
