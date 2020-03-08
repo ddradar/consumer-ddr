ba44d7f60f17751018bff48c85e6c3e0e2180c9a
nogic <24802730+nogic1008@users.noreply.github.com>
2020-03-08 10:38:28 +0900

    feat: add 2nd ReMIX json

--- series/index.ts
+++ series/index.ts
@@ -1,4 +1,5 @@
 import firstJp from './1st-jp.json'
+import secondRemixJp from './2nd-remix-jp.json'
 
-const softwareList = [firstJp]
+const softwareList = [firstJp, secondRemixJp]
 export default softwareList
