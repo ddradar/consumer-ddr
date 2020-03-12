fae820fab980f70dcede2943ed1fd12090f902b6
Nogic <24802730+nogic1008@users.noreply.github.com>
2020-03-12 22:25:37 +0900

    feat: song info & detail (#49)
    * feat: create song-repository
    * feat: add song list page
    * feat: add song detail page
    * feat: add song/_id route

---
+++ __tests__/pages/song/index.test.ts
@@ -0,0 +1,144 @@
+import {
+  createLocalVue,
+  mount,
+  RouterLinkStub,
+  shallowMount,
+  Wrapper
+} from '@vue/test-utils'
+import Buefy from 'buefy'
+import { mocked } from 'ts-jest/utils'
+import { MetaInfo } from 'vue-meta'
+
+import SongList from '~/pages/song/index.vue'
+import { getSoftwareList } from '~/plugins/software-repository'
+import { getSongList } from '~/plugins/song-repository'
+
+const localVue = createLocalVue()
+localVue.use(Buefy)
+jest.mock('~/plugins/software-repository')
+jest.mock('~/plugins/song-repository')
+mocked(getSoftwareList).mockReturnValue([
+  {
+    id: '1st-jp',
+    name: 'Dance Dance Revolution',
+    region: 'JP',
+    platform: 'Play Station',
+    launched: new Date('1999-04-10')
+  },
+  {
+    id: '2nd-remix-jp',
+    name: 'Dance Dance Revolution 2nd ReMIX',
+    platform: 'Play Station',
+    region: 'JP',
+    launched: new Date('1999-08-26')
+  }
+])
+mocked(getSongList).mockReturnValue([
+  {
+    id: 'am-3p',
+    name: 'AM-3P',
+    artist: 'KTz',
+    bpm: 130,
+    softwareIds: ['2nd-remix-jp'],
+    charts: [
+      {
+        playStyle: 'SINGLE',
+        difficulty: 1,
+        level: [5]
+      },
+      {
+        playStyle: 'SINGLE',
+        difficulty: 2,
+        level: [6]
+      },
+      {
+        playStyle: 'SINGLE',
+        difficulty: 3,
+        level: [8]
+      },
+      {
+        playStyle: 'DOUBLE',
+        difficulty: 1,
+        level: [5]
+      },
+      {
+        playStyle: 'DOUBLE',
+        difficulty: 2,
+        level: [6]
+      }
+    ]
+  },
+  {
+    id: 'paranoia',
+    name: 'PARANOiA',
+    artist: '180',
+    bpm: 180,
+    softwareIds: ['1st-jp', '2nd-remix-jp'],
+    charts: [
+      {
+        playStyle: 'SINGLE',
+        difficulty: 1,
+        level: [6]
+      },
+      {
+        playStyle: 'SINGLE',
+        difficulty: 2,
+        level: [7]
+      }
+    ]
+  },
+  {
+    id: 'paranoia',
+    name: 'PARANOiA',
+    artist: '180',
+    bpm: 180,
+    softwareIds: ['1st-jp', '2nd-remix-jp'],
+    charts: [
+      {
+        playStyle: 'SINGLE',
+        difficulty: 1,
+        level: [6]
+      },
+      {
+        playStyle: 'SINGLE',
+        difficulty: 2,
+        level: [7]
+      }
+    ]
+  }
+])
+
+describe('pages/song/index.vue', () => {
+  let wrapper: Wrapper<Vue>
+
+  beforeEach(() => {
+    wrapper = shallowMount(SongList, {
+      localVue,
+      stubs: {
+        NuxtLink: RouterLinkStub
+      }
+    })
+  })
+
+  test('is a Vue instance', () => {
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+  test('renders correctly', () => {
+    const wrapper = mount(SongList, {
+      localVue,
+      stubs: {
+        NuxtLink: RouterLinkStub
+      }
+    })
+    expect(wrapper.element).toMatchSnapshot()
+  })
+  describe('head', () => {
+    test('returns "Song List"', () => {
+      // Arrange
+      const head = wrapper.vm.$data.head as MetaInfo
+
+      // Act & Assert
+      expect(head).toStrictEqual({ title: 'Song List' })
+    })
+  })
+})
---
+++ __tests__/plugins/song-repository.test.ts
@@ -0,0 +1,138 @@
+import softwareList from '~/series'
+
+describe('plugins/song-repository.ts', () => {
+  const mockSoftware: typeof softwareList = [
+    {
+      id: '1st-jp',
+      name: 'Dance Dance Revolution',
+      region: 'JP',
+      launched: '1999-04-10',
+      platform: 'Play Station',
+      songs: [
+        {
+          id: 'have-you-never-been-mellow',
+          name: 'HAVE YOU NEVER BEEN MELLOW',
+          artist: 'THE OLIVIA PROJECT',
+          bpm: 126,
+          charts: [
+            {
+              playStyle: 'SINGLE',
+              difficulty: 1,
+              level: 1
+            }
+          ]
+        },
+        {
+          id: 'paranoia-180',
+          name: 'PARANOiA',
+          artist: '180',
+          bpm: 180,
+          charts: [
+            {
+              playStyle: 'SINGLE',
+              difficulty: 1,
+              level: 7
+            },
+            {
+              playStyle: 'SINGLE',
+              difficulty: 2,
+              level: 7
+            }
+          ]
+        }
+      ],
+      difficultyNames: {}
+    },
+    {
+      id: 'supernova2-jp',
+      name: 'Dance Dance Revolution SuperNOVA2',
+      region: 'JP',
+      launched: '2017-02-01',
+      platform: 'Play Station 2',
+      songs: [
+        {
+          id: 'paranoia-180',
+          name: 'PARANOiA',
+          artist: '180',
+          bpm: 180,
+          charts: [
+            {
+              playStyle: 'SINGLE',
+              difficulty: 1,
+              level: 6
+            },
+            {
+              playStyle: 'SINGLE',
+              difficulty: 2,
+              level: 7
+            },
+            {
+              playStyle: 'DOUBLE',
+              difficulty: 1,
+              level: 7
+            }
+          ]
+        }
+      ],
+      difficultyNames: {}
+    }
+  ]
+
+  beforeEach(() => {
+    jest.resetModules()
+    jest.mock('~/series', () => mockSoftware)
+  })
+
+  describe('getSongList()', () => {
+    test('returns SongInfo[]', async () => {
+      // Act
+      const list = (await import('~/plugins/song-repository')).getSongList()
+
+      // Assert
+      expect(list).toHaveLength(2)
+    })
+  })
+  describe('getSongInfo()', () => {
+    test('returns undefined if not found', async () => {
+      // Act
+      const info = (await import('~/plugins/song-repository')).getSongInfo(
+        'foo'
+      )
+
+      // Assert
+      expect(info).toBe(undefined)
+    })
+    test('returns SongInfo if found', async () => {
+      // Act
+      const info = (await import('~/plugins/song-repository')).getSongInfo(
+        'paranoia-180'
+      )
+
+      // Assert
+      expect(info).toStrictEqual({
+        id: 'paranoia-180',
+        name: 'PARANOiA',
+        artist: '180',
+        bpm: 180,
+        softwareIds: ['1st-jp', 'supernova2-jp'],
+        charts: [
+          {
+            playStyle: 'SINGLE',
+            difficulty: 1,
+            level: [7, 6]
+          },
+          {
+            playStyle: 'SINGLE',
+            difficulty: 2,
+            level: [7]
+          },
+          {
+            playStyle: 'DOUBLE',
+            difficulty: 1,
+            level: [7]
+          }
+        ]
+      })
+    })
+  })
+})
