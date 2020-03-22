0274665c15df3efe59ac09994edee553f8570bc4
nogic <24802730+nogic1008@users.noreply.github.com>
2020-03-22 13:26:20 +0900

    feat(software): add Dance Dance Revolution GB2

--- series/index.ts
+++ series/index.ts
@@ -9,9 +9,10 @@ import best from './best.json'
 import club1 from './club-1.json'
 import club2 from './club-2.json'
 import clubDc from './club-dc.json'
-import dsDkt from './dct.json'
+import dsDct from './dct.json'
 import disneyJp from './disney-jp.json'
 import gb1 from './gb-1.json'
+import gb2 from './gb-2.json'
 import dsTkd from './tkd.json'
 
 type SoftWareJson = {
@@ -25,17 +26,18 @@ type SoftWareJson = {
 }
 
 const softwareList = [
-  firstJp,
-  second,
-  club1,
-  dsTkd,
-  club2,
-  secondDc,
-  dsDkt,
-  clubDc,
-  third,
-  gb1,
-  disneyJp,
-  best
+  firstJp, // 1999-04-10
+  second, // 1999-08-26
+  club1, // 1999-11-25
+  dsTkd, // 1999-12-09
+  club2, // 1999-12-22
+  secondDc, // 2000-02-17
+  dsDct, // 2000-04-20
+  clubDc, // 2000-04-27
+  third, // 2000-06-01
+  gb1, // 2000-08-03
+  gb2, // 2000-11-16
+  disneyJp, // 2000-11-30
+  best // 2000-12-21
 ] as SoftWareJson[]
 export default softwareList
