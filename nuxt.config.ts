a7e572fc91cda9f3767d891b9866ca6e99bd317c
Nogic <24802730+nogic1008@users.noreply.github.com>
2021-03-09 11:15:06 +0900

    build: auto generate icon files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,36 +8,25 @@ const config: NuxtConfig = {
   target: 'static',
   head: {
     titleTemplate: `%s - ${title}`,
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
-    ],
-    link: [
-      { rel: 'icon', sizes: '16x16', type: 'image/png', href: '/icon-16.png' },
-      { rel: 'icon', sizes: '32x32', type: 'image/png', href: '/icon-32.png' },
-      { rel: 'icon', sizes: '48x48', type: 'image/png', href: '/icon-48.png' },
-      {
-        rel: 'apple-touch-icon',
-        sizes: '180x180',
-        href: '/apple-touch-icon.png'
-      },
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
   loading: { color: '#fff' },
   css: ['~/assets/css/styles.scss'],
   pwa: {
-    icon: {
-      iconFileName: 'icon-1024.png'
-    },
     manifest: {
       name: title,
       short_name: 'CS DDR',
       description: pkg.description,
       background_color: '#fff',
       theme_color: '#7957d5',
-      orientation: 'any'
+      orientation: 'any',
+      start_url: '/'
+    },
+    meta: {
+      name: title,
+      description: pkg.description,
+      theme_color: '#7957d5',
+      twitterCard: 'summary'
     }
   },
   buildModules: [
