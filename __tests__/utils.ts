5b444edfde96e5e03e16a16d0f23e4b1f5586b23
Nogic <24802730+nogic1008@users.noreply.github.com>
2022-08-10 01:27:32 +0000

    test: update snapshot

--- __tests__/utils.ts
+++ __tests__/utils.ts
@@ -1,11 +1,9 @@
 import Oruga from '@oruga-ui/oruga'
 import { bulmaConfig } from '@oruga-ui/theme-bulma'
 import { createLocalVue } from '@vue/test-utils'
-import Buefy from 'buefy'
 
 export function createVue() {
   const localVue = createLocalVue()
-  localVue.use(Buefy)
   localVue.use(Oruga, bulmaConfig)
   return localVue
 }
