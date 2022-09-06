54600a93a709fe38920cc1518d8e0ba164cf3069
Nogic <24802730+nogic1008@users.noreply.github.com>
2022-09-06 01:59:30 +0000

    feat(software): add color property

--- composables/useSoftwareList.ts
+++ composables/useSoftwareList.ts
@@ -2,7 +2,14 @@ import type { Ref } from 'vue'
 
 import type { SoftwareParsedContent } from '~~/src/content'
 
-const keys = ['slug', 'title', 'platform', 'region', 'launched'] as const
+const keys = [
+  'slug',
+  'title',
+  'color',
+  'platform',
+  'region',
+  'launched'
+] as const
 export type SoftwareListData = Pick<SoftwareParsedContent, typeof keys[number]>
 
 export default async function () {
