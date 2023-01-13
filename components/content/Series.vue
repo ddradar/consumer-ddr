d07438fd9ea6a27dd861136544a7047d4bd29705
Nogic <24802730+nogic1008@users.noreply.github.com>
2023-01-13 09:20:52 +0900

    fix(component): use internal color

--- components/content/Series.vue
+++ components/content/Series.vue
@@ -3,14 +3,47 @@
     :is="tag === 'NuxtLink' ? NuxtLink : tag"
     :style="{ backgroundColor, color: '#fff' }"
   >
-    <span :title="software.title">{{ slug }}</span>
+    <span>{{ slug }}</span>
   </Component>
 </template>
 
 <script lang="ts" setup>
-import useSoftwareData from '~~/composables/useSoftwareData'
-
 const NuxtLink = resolveComponent('NuxtLink')
+const colors = {
+  '1st-jp': 'darkviolet',
+  '2nd-dc': 'darkorange',
+  '2nd': 'mediumblue',
+  '3rd': 'crimson',
+  '4th': 'midnightblue',
+  '5th': 'orange',
+  best: 'goldenrod',
+  'club-1': 'deepskyblue',
+  'club-2': 'fuchsia',
+  'club-dc': 'royalblue',
+  dct: 'mediumvioletred',
+  'disney-jp': 'dimgray',
+  'disney-us': 'gold',
+  'ext-jp': 'green',
+  'ext-us': 'seagreen',
+  ext2: 'seagreen',
+  extra: 'deeppink',
+  festival: 'seagreen',
+  'gb-disney': 'orchid',
+  gb: 'tomato',
+  gb2: 'darkgreen',
+  gb3: 'goldenrod',
+  konamix: 'orange',
+  'max-jp': 'blue',
+  'max-us': 'darkorchid',
+  'max2-jp': 'black',
+  'max2-us': 'orange',
+  'oha-gb': 'darkgray',
+  oha: 'dodgerblue',
+  party: 'hotpink',
+  strike: 'dodgerblue',
+  tkd: 'midnightblue',
+  us: 'mediumvioletred',
+}
 
 interface SeriesButtonProps {
   slug: string
@@ -18,6 +51,5 @@ interface SeriesButtonProps {
 }
 
 const prop = defineProps<SeriesButtonProps>()
-const { software } = await useSoftwareData(prop.slug)
-const backgroundColor = computed(() => software.value.color ?? '#3e8ed0')
+const backgroundColor = colors[prop.slug] ?? '#3e8ed0'
 </script>
