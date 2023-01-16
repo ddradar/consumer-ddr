baa9e061e961db086e3bdad6422e77a5bfb21829
Nogic <24802730+nogic1008@users.noreply.github.com>
2023-01-16 14:35:00 +0900

    style: fix format

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,56 +16,56 @@ export default defineNuxtConfig({
   nitro: {
     prerender: {
       routes: [
-        ...series.map(s => `/songs/${s}`),
-        ...songs.map(s => `/songs/${s}`),
-      ],
-    },
+        ...series.map(s => `/series/${s}`),
+        ...songs.map(s => `/songs/${s}`)
+      ]
+    }
   },
   app: {
     head: {
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: pkg.description },
+        { hid: 'description', name: 'description', content: pkg.description }
       ],
       link: [
         {
           rel: 'icon',
           sizes: '16x16',
           type: 'image/png',
-          href: '/icon-16.png',
+          href: '/icon-16.png'
         },
         {
           rel: 'icon',
           sizes: '32x32',
           type: 'image/png',
-          href: '/icon-32.png',
+          href: '/icon-32.png'
         },
         {
           rel: 'icon',
           sizes: '48x48',
           type: 'image/png',
-          href: '/icon-48.png',
+          href: '/icon-48.png'
         },
         {
           rel: 'apple-touch-icon',
           sizes: '180x180',
-          href: '/apple-touch-icon.png',
+          href: '/apple-touch-icon.png'
         },
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
         {
           rel: 'stylesheet',
-          href: 'https://cdn.jsdelivr.net/npm/@mdi/font@5.x/css/materialdesignicons.min.css',
-        },
-      ],
-    },
+          href: 'https://cdn.jsdelivr.net/npm/@mdi/font@5.x/css/materialdesignicons.min.css'
+        }
+      ]
+    }
   },
   css: ['~/assets/css/main.scss'],
   build: {
-    transpile: [/@oruga-ui\/oruga-next/],
+    transpile: [/@oruga-ui\/oruga-next/]
   },
   modules: [
     // Doc: https://content.nuxtjs.org/
-    '@nuxt/content',
+    '@nuxt/content'
   ],
 })
