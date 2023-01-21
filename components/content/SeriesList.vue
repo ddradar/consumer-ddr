17a6ef9f9090694722e4e1307db4ca9b3f372464
nogic <24802730+nogic1008@users.noreply.github.com>
2023-01-21 16:35:45 +0900

    fix: fix test & series-list component

--- components/content/SeriesList.vue
+++ components/content/SeriesList.vue
@@ -1,12 +1,13 @@
 <template>
   <ul>
-    <li v-for="v in series" :key="v.slug">
-      <NuxtLink :to="`/series/${v.slug}/`">{{ v.title }}</NuxtLink>
+    <li v-for="slug in series" :key="slug">
+      <NuxtLink :to="`/series/${slug}/`">{{ seriesList[slug].title }}</NuxtLink>
     </li>
   </ul>
 </template>
 
 <script lang="ts" setup>
+import seriesList from '~~/content/series/.seriesList.json'
 import type { Song } from '~~/src/content'
 
 const NuxtLink = resolveComponent('NuxtLink')
