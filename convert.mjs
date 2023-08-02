70bd2575f7abb654bc67e7d789b95789bebe3565
nogic <24802730+nogic1008@users.noreply.github.com>
2023-08-03 08:46:46 +0900

    chore(deps): update docus

--- convert.mjs
+++ convert.mjs
@@ -1,9 +1,10 @@
-import {  load } from 'js-yaml'
+import { load } from 'js-yaml'
 import { readFile, readdir, writeFile } from 'node:fs/promises'
 import { join } from 'node:path'
 
-const srcFolder = './content/.songs/disney-jp'
-const destFolder = './content/playstation-jp/disney'
+// ,/windows/grand-prix" level="(.+?)(,.+?)*,(.+?)"\} " level="$1"} :levels{links="/windows/grand-prix" level="$3"}
+const srcFolder = './content/.songs/nova2-us'
+const destFolder = './content/playstation2-us/supernova2'
 const files = await readdir(srcFolder)
 for (const file of files) {
   const fileText = await readFile(join(srcFolder, file), { encoding: 'utf-8' })
@@ -15,6 +16,8 @@ for (const file of files) {
   for (const [key, value] of Object.entries(mdcContent)) {
     convertedText = convertedText.replaceAll(`{{ $doc.${key} }}`, value)
   }
+  convertedText = convertedText.replaceAll(':series-list{:series="series"}', fileText.match(/series:\n(.+?)-{3}/ms)[1])
+  convertedText = convertedText.replace(/^-{3}(.+?)-{3}\n\n/ms, '')
 
   await writeFile(join(destFolder, file), convertedText, { encoding: 'utf-8' })
 }
